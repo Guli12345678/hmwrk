04e7c5cfb7529845ce2b5bd7c30149250b48e1f6
Guli12345678 <gulsanammamadieva083@gmail.com>
2025-04-09 17:50:28 +0500

    change

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("Salom NodeJS");
+// //-> majpr, minor, patch -> nodemon 3.1.9
+// // loyihaning ichidagida dependencies -> dasturga eng kerak boladigan -> devdependies
+// // main -> dasturga kirish nuqtasi
+// // script
+
+// const cowsay = require("cowsay");
+// console.log(
+//   cowsay.say({
+//     text: "Salom NodeJS",
+//     e: "$$",
+//     T: " U",
+//     // r: true,
+//   })
+// );
+// const gulsanam = require("gulsanam-mamadiyeva");
+// console.log(gulsanam);
+const cyrtolat = require("cyr-to-lat-uz");
+const translate = cyrtolat();
+console.log(translate.fromcyrtolat("кок кок "));
